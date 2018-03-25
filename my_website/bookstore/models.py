0590a02785c4d3e8af36bbd32d32f370b378fcd2
Eric-LiuXin <liuxin_coding@sina.com>
2018-03-25 10:30:37 +0800

    Add book model

--- my_website/bookstore/models.py
+++ my_website/bookstore/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Books(models.Model):
+    name = models.CharField(max_length=200)
+    cover = models.CharField(max_length=200)
+    create_date = models.DateField(auto_now=True)
+    intro = models.TextField(max_length=200)
+    down_link = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ('-create_date',)
+
+    def __unicode__(self):
+       return self.name
